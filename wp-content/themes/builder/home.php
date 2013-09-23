9ce82e3492dafeabdfb8e30a9d16bd070c290bb5
&y <andywarthol@gmail.com>
2013-09-22 19:46:47 -0700

    Advanced Custom Fields Home Page Slider

--- wp-content/themes/builder/home.php
+++ wp-content/themes/builder/home.php
@@ -10,44 +10,49 @@ homepage of your website.
 ?>
 <?php get_header(); ?>
 
+<section id="hero">
+	
+</section>
 <div id="banner" class="home-page">
-	<div class="container">
-		<div id="inner-content" class="wrap clearfix">
-			<div class="jumbotron">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-7">
-							<h1>Sports, Life, Dance</h1>
-							<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-							<p><a class="btn btn-primary btn-lg">Learn more</a></p>
-						</div>
-						<div class="col-lg-5">
-							<p><img src="<?php echo get_template_directory_uri(); ?>/library/images/flat.png"></p>
-						</div>
-					</div><!-- end .row-->
-				</div> <!-- end .container-->
-			</div> <!-- end .jumbotron-->
-		</div> <!-- end #inner-content -->
-	</div> <!-- end .container-->
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<div class="item active sports">
+				<div class="jumbotron">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12 stage light">
+								<h1>Sports</h1>
+							</div>
+							<div class="col-lg-5">
+								
+							</div>
+						</div><!-- end .row-->
+					</div> <!-- end .container-->
+				</div> <!-- end .jumbotron-->
+			</div>
+		</div>
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+	</div><!-- end of carousel -->
 </div> <!-- end #banner-->
 
 <div class="container home-three">
 	<div class="row">
 		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
+			<img class="" src="http://placehold.it/320x240">
+			<h2>Compelling Headline 1</h2>
 			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
 		</div><!-- /.col-lg-4 -->
 		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
+			<img class="" src="http://placehold.it/320x240">
+			<h2>Compelling Headline 2</h2>
 			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
 		</div><!-- /.col-lg-4 -->
 		<div class="col-lg-4">
-			<img class="img-circle" src="http://placehold.it/140x140">
-			<h2>Heading</h2>
+			<img class="" src="http://placehold.it/320x240">
+			<h2>Compelling Headline 3</h2>
 			<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 			<p><a class="btn btn-success" href="#">View details <span class="glyphicon glyphicon-chevron-right"></span></a></p>
 		</div><!-- /.col-lg-4 -->
