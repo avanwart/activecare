571fbd3fbeb50e2ecd7da9e9e074f63fc368b991
&y <andywarthol@gmail.com>
2013-09-22 21:08:29 -0700

    Simplifying the carousel Favicon

--- wp-content/themes/builder/home.php
+++ wp-content/themes/builder/home.php
@@ -30,6 +30,34 @@ homepage of your website.
 					</div> <!-- end .container-->
 				</div> <!-- end .jumbotron-->
 			</div>
+			<div class="item life">
+				<div class="jumbotron">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12 stage light">
+								<h1>Life</h1>
+							</div>
+							<div class="col-lg-5">
+								
+							</div>
+						</div><!-- end .row-->
+					</div> <!-- end .container-->
+				</div> <!-- end .jumbotron-->
+			</div>
+			<div class="item dance">
+				<div class="jumbotron">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12 stage light">
+								<h1>Dance</h1>
+							</div>
+							<div class="col-lg-5">
+								
+							</div>
+						</div><!-- end .row-->
+					</div> <!-- end .container-->
+				</div> <!-- end .jumbotron-->
+			</div>
 		</div>
 		<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
 		<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
